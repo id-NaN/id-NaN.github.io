51725cbff399c8571af635d77f3b2abd7a68878b
Morgan Kuhles <48955787+id-NaN@users.noreply.github.com>
2020-07-11 22:39:00 +0200

    new file:   color_convert.js 	new file:   main.html 	new file:   main.js 	new file:   segments.js 	new file:   styles.css

---
+++ main.js
@@ -0,0 +1,95 @@
+let segments = {
+	"plain text":text_plain,
+	"colored text":text_colored,
+	"rainbow text":text_rainbow,
+}
+
+
+
+let message = []
+
+
+
+function load_data() {
+	let dropdown_content = ""
+	let segment_types = Object.keys(segments)
+	for (var i = 0; i <= segment_types.length - 1; i++) {
+		let segment_type = segment_types[i];
+		dropdown_content += "<a onclick=\"new_segment(" + i + ")\" class=\"unselectable\">" + segment_type + "</a>"
+	}
+	let dropdown = document.querySelectorAll("#select_segment_dropdown .dropdown_content");
+	dropdown[0].innerHTML = dropdown_content;
+}
+
+
+
+function update_segment_display() {
+	let segment_html = "";
+	for (var i = 0; i < message.length; i++) {
+		segment = message[i];
+		segment_html +=
+			"<div class=\"segment_container\">" + segment.get_html() + "</div>"
+	}
+	let container = document.getElementById("main_segment_container");
+	container.innerHTML = segment_html;
+	for (var i = container.children.length - 1; i >= 0; i--) {
+		child = container.children[i];
+		child.setAttribute("oninput", "process_data()");
+	}
+}
+
+
+
+function update_result_display(argument) {
+	let preview_segments = []
+	let result_segments = []
+	for (var i = 0; i < message.length; i++) {
+		let segment = message[i]
+		preview_segments.push(segment.get_preview())
+		result_segments.push(segment.get_result())
+	}
+	let message_preview = preview_segments.join("")
+	let message_result = result_segments.join("")
+	document.getElementById("preview_area").innerHTML = message_preview
+	document.getElementById("result_area").value = message_result
+}
+
+
+
+function reset_editor() {
+	message = []
+	update_segment_display()
+	update_result_display()
+}
+
+
+
+function new_segment(segment_index) {
+	let keys = Object.keys(segments)
+	message.push(new segments[keys[segment_index]])
+	console.log(message)
+	update_segment_display()
+}
+
+
+
+function process_data() {
+	let main_container = document.getElementById("main_segment_container");
+	for (var i = 0; i < main_container.children.length; i++) {
+		let segment_container = main_container.children[i]
+		for (var j = 0; j < segment_container.children.length; j++) {
+			let element = segment_container.children[j]
+			if (element.classList.contains("input")) {
+				message[i][element.id] = element.value
+			}
+		}
+	}
+	update_result_display()
+}
+
+
+
+function copy(copy_text) {
+	copy_text.select();
+	document.execCommand("copy");
+}
---
+++ segments.js
@@ -0,0 +1,96 @@
+String.prototype.format = function() {
+	var args = arguments;
+	return this.replace(/{(\d+)}/g, function(match, number) { 
+		return typeof args[number] != 'undefined'
+			? args[number]
+			: match
+		;
+	});
+};
+
+
+
+
+class text_plain{
+	constructor(){
+		this.text = ""
+		this.base_html = "<p>plain text</p>\
+				<textarea id=\"text\" class=\"input\" style=\"width:300px;\">{0}</textarea>"
+	}
+	get_html(){
+		return this.base_html.format(this.text)
+	}
+	get_preview(){
+		return this.text
+	}
+	get_result(){
+		return this.text
+	}
+}
+
+
+
+class text_colored{
+	constructor(){
+		this.text = ""
+		this.color = "#000000"
+		this.base_html = "<p>colored text</p>\
+				<textarea id=\"text\" class=\"input\" style=\"width:300px;\">{0}</textarea>\
+				<input type=\"color\" id=\"color\" class=\"input\" value=\"{1}\"></input>"
+		this.preview_html = "<span style=\"color:{1};\">{0}</span>"
+	}
+	get_html(){
+		return this.base_html.format(this.text, this.color)
+	}
+	get_preview(){
+		return this.preview_html.format(this.text, this.color)
+	}
+	get_result(){
+		return "[c/{0}:{1}]".format(this.color.slice(1), this.text)
+	}
+}
+
+
+
+class text_rainbow{
+	constructor(){
+		this.text = ""
+		this.color = "#000000"
+		this.offset = 0
+		this.base_html = "<p>colored text</p>\
+				<textarea id=\"text\" class=\"input\" style=\"width:300px;\">{0}</textarea>\
+				<input type=\"color\" id=\"color\" class=\"input\" value=\"{1}\"></input>\
+				<input type=\"number\" id=\"offset\" class=\"input\" value=\"{2}\"></input>"
+		this.preview_html = "<span style=\"color:{1};\">{0}</span>"
+	}
+	get_html(){
+		return this.base_html.format(this.text, this.color, this.offset)
+	}
+	get_preview(){
+		let preview = ""
+		let color_hsl = hexToHSL(this.color)
+		for (var i = 0; i < this.text.length; i++) {
+			let letter = this.text[i]
+			preview += this.preview_html.format(letter, HSLToHex(color_hsl))
+			color_hsl[0] += parseInt(this.offset)
+			if (color_hsl[0] >= 360){
+				color_hsl[0] = 0
+			}
+		}
+		return preview
+	}
+	get_result(){
+		let result = ""
+		let color_hsl = hexToHSL(this.color)
+		for (var i = 0; i < this.text.length; i++) {
+			let letter = this.text[i]
+			result += "[c/{0}:{1}]".format(HSLToHex(color_hsl).slice(1), letter)
+			color_hsl[0] += parseInt(this.offset)
+			if (color_hsl[0] >= 360){
+				color_hsl[0] = 0
+			}
+		}
+		return result
+		return "[c/{0}:{1}]".format(this.color.slice(1), this.text)
+	}
+}
